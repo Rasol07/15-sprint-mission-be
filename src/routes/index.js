f5484f3b76bef88bbbe04cca933622f1a45b22b6
Rasol07 <poloa0709@gmail.com>
2026-08-31 01:15:22 +0900

    feat: 서버 health-check추가

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,4 +2,7 @@ import express from 'express';
 import { productRouter } from './products_route.js';
 export const router = express.Router();
 
+router.get('/', (req, res) => {
+  res.status(200).send('서버가 정상적으로 실행 중입니다.');
+});
 router.use('/products', productRouter);
